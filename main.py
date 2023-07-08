4e4e595964f0ff2e2bec08765bce6d53d6228474
tangjie <tangjie1953479@tongji.edu.cn>
2023-07-08 18:30:19 +0800

    20230708: main.py finished

--- main.py
+++ main.py
@@ -2,33 +2,35 @@ import os
 import pickle
 import numpy as np
 import warnings
-from utils import RealDataLoader,SimuDataLoader,XsenDataLoader,MixData
+from utils import RealDataLoader, SimuDataLoader, XsenDataLoader, MobiFallDataLoader, MixData
 
-warnings.filterwarnings('ignore',category=FutureWarning)
+warnings.filterwarnings('ignore', category=FutureWarning)
 import tensorflow as tf
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-TB_DIR="for_tensorboard"
-model_dir="saved_model"
+TB_DIR = "for_tensorboard"
+model_dir = "saved_model"
+
 
 class FallDetector:
-    def __init__(self,batch_size,num_features,timesteps,num_classes):
-        self.batch_size=batch_size
-        self.num_features=num_features
-        self.timesteps=timesteps
-        self.num_classes=num_classes
+    def __init__(self, batch_size, num_features, timesteps, num_classes):
+        self.batch_size = batch_size
+        self.num_features = num_features
+        self.timesteps = timesteps
+        self.num_classes = num_classes
 
-        os.makedirs(model_dir,exist_ok=True)
-        self.model_file=os.path.join(model_dir,"model.ckpt")
+        os.makedirs(model_dir, exist_ok=True)
+        self.model_file = os.path.join(model_dir, "model.ckpt")
 
         print("Resetting the default graph...")
         self.build_model()
         self.define_loss()
         self.define_optimizer()
 
-        self.saver=tf.train.Saver()
-        self.tensorboard_op=tf.summary.merge_all()
+        self.saver = tf.train.Saver()
+        self.tensorboard_op = tf.summary.merge_all()
         print("Model built!")
 
     def build_model(self):
@@ -36,101 +38,253 @@ class FallDetector:
         tf.compat.v1.reset_default_graph()
 
         # define placeholders
-        self.x=tf.compat.v1.placeholder(tf.float32,shape=(self.batch_size,self.timesteps,self.num_features),name='x')
-        self.y=tf.compat.v1.placeholder(tf.float32,shape=(self.batch_size,self.num_classes),name='y')
-        self.is_training=tf.compat.v1.placeholder(tf.bool,name='is_training')
+        self.x = tf.compat.v1.placeholder(tf.float32, shape=(self.batch_size, self.timesteps, self.num_features),
+                                          name='x')
+        self.y = tf.compat.v1.placeholder(tf.float32, shape=(self.batch_size, self.num_classes), name='y')
+        self.is_training = tf.compat.v1.placeholder(tf.bool, name='is_training')
 
         # normalize input
-        normalized_input=tf.layers.batch_normalization(self.x,axis=2,training=self.is_training)
+        normalized_input = tf.layers.batch_normalization(self.x, axis=2, training=self.is_training)
 
         # convert input tensor to list of tensors
-        inputs=tf.split(normalized_input,self.timesteps,axis=1)
-        inputs=[tf.squeeze(inp) for inp in inputs]
+        inputs = tf.split(normalized_input, self.timesteps, axis=1)
+        inputs = [tf.squeeze(inp) for inp in inputs]
 
         # RNN layers
-        rnn1_out=self._lstm_layer(inputs,32,"lstm-1")
-        rnn2_out=self._lstm_layer(rnn1_out,64,"lstm-2")
+        rnn1_out = self._lstm_layer(inputs, 32, "lstm-1")
+        rnn2_out = self._lstm_layer(rnn1_out, 64, "lstm-2")
 
         # Get last node
-        final_node=rnn2_out[-1]
+        final_node = rnn2_out[-1]
 
         # Dense layers
-        dense1_out=self._dense_layer(final_node,32,"dense-1",activation=tf.nn.elu)
-        dense2_out=self._dense_layer(dense1_out,self.num_classes,"dense-2",activation=tf.nn.softmax)
+        dense1_out = self._dense_layer(final_node, 32, "dense-1", activation=tf.nn.elu)
+        dense2_out = self._dense_layer(dense1_out, self.num_classes, "dense-2", activation=tf.nn.softmax)
 
-        self.prefiction=dense2_out
+        self.prediction = dense2_out
 
         # Accuracy
-        pred_classes=tf.argmax(self.prefiction,axis=-1)
-        actual_classes=tf.argmax(self.y,axis=-1)
-        correct = tf.equal(pred_classes,actual_classes)
-        self.accuracy=tf.reduce_mean(tf.cast(correct,tf.float32),name='accuracy')
+        pred_classes = tf.argmax(self.prediction, axis=-1)
+        actual_classes = tf.argmax(self.y, axis=-1)
+        correct = tf.equal(pred_classes, actual_classes)
+        self.accuracy = tf.reduce_mean(tf.cast(correct, tf.float32), name='accuracy')
 
     def define_loss(self):
-        self.loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.y,logits=self.prefiction))
-        tf.summary.scalar("Cross-entropy-loss",self.loss)
+        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.y, logits=self.prediction))
+        tf.summary.scalar("Cross-entropy-loss", self.loss)
 
     def define_optimizer(self):
-        optimizer=tf.train.AdamOptimizer(0.0001)
-        update_ops=tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        optimizer = tf.train.AdamOptimizer(0.0001)
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         with tf.control_dependencies(update_ops):
-            self.train_step=optimizer.minimize(self.loss)
+            self.train_step = optimizer.minimize(self.loss)
 
-    def _lstm_layer(self,inputs,num_units,scope,cell_type="LSTM"):
+    def _lstm_layer(self, inputs, num_units, scope, cell_type="LSTM"):
         with tf.variable_scope(scope):
-            if cell_type=="LSTM":
-                lstm_cell=tf.nn.rnn_cell.LSTMCell(num_units)
-            elif cell_type=="GRU":
+            if cell_type == "LSTM":
+                lstm_cell = tf.nn.rnn_cell.LSTMCell(num_units)
+            elif cell_type == "GRU":
                 lstm_cell = tf.nn.rnn_cell.GRUCell(num_units)
             else:
                 raise NotImplementedError("{} type not implemented.".format(cell_type))
 
-            outputs,states=tf.nn.static_rnn(lstm_cell,inputs,dtype=tf.float32)
+            outputs, states = tf.nn.static_rnn(lstm_cell, inputs, dtype=tf.float32)
         return outputs
 
-    def _dense_layer(self,inputs,num_units,scope,activation=None):
+    def _dense_layer(self, inputs, num_units, scope, activation=None):
         with tf.variable_scope(scope):
-            w=tf.get_variable("w",shape=(inputs.shape[-1].value,num_units))
-            b=tf.get_variable("b",shape=(num_units),initializer=tf.constant_initializer(0.1))
+            w = tf.get_variable("w", shape=(inputs.shape[-1].value, num_units))
+            b = tf.get_variable("b", shape=(num_units), initializer=tf.constant_initializer(0.1))
 
-            out=tf.matmul(inputs,w)+b
+            out = tf.matmul(inputs, w) + b
             if activation is not None:
-                out=activation(out)
+                out = activation(out)
 
         return out
 
-    def train(self,path,epochs=50,log_step=10,resume=False,dataloader=""):
+    def load_dataset(self, path, dataloader, use_type):
+        if os.path.isfile(f"{use_type}_dataset.pkl"):
+            os.remove(f"{use_type}_dataset.pkl")
+
+        loader_map = {"simu": SimuDataLoader,
+                      "real": RealDataLoader,
+                      "xsen": XsenDataLoader,
+                      "mobifall": MobiFallDataLoader}
+
+        if dataloader in loader_map.keys():
+            data = loader_map[dataloader](path)
+            pickle.dump(data, open(f"{use_type}_dataset.pkl", 'wb'))
+        else:
+            print("Error! Unknown data loader.")
+
+    def train(self, epochs=50, log_step=10, resume=False):
         print("Beginning the training process...")
 
-        loader_class =  {"simu":SimuDataLoader(path),
-                        "real":RealDataLoader(path),
-                        "xsen":XsenDataLoader(path)}
+        data = pickle.load(open("train_dataset.pkl", 'rb'))
 
-        if not path:
-            if not os.path.isfile("train_dataset.pkl"):
-                print("Error!No dataset to load.")
-
-        if os.path.isfile("train_dataset.pkl"):
-            data=pickle.load(open("train_dataset.pkl",'rb'))
-        elif dataloader in loader_class.keys():
-            data=loader_class.get(dataloader)
-            pickle.dump(data,open("train_dataset.pkl",'wb'))
+        valid_file = os.path.join(model_dir, "min_valid_loss.txt")
+        if os.path.isfile(valid_file):
+            min_valid_loss = float(open(valid_file).read().strip())
         else:
-            print("Error!Unknown data loader.")
+            min_valid_loss = 1000000.0
+
+        with tf.Session() as session:
+            writer = tf.summary.FileWriter(TB_DIR, session.graph)
+
+            if resume:
+                try:
+                    self.saver.restore(session, self.model_file)
+                    print("Previous checkpoint file found,continue training...")
+                except:
+                    print("No previous checkpoint file found,restarting training...")
+
+            else:
+                min_valid_loss = 1000000.0
+                session.run(tf.global_variables_initializer())
+
+            for e in range(epochs):
+                avg_loss = []
+                for batch_x, batch_y in data.dataset.next_batch(self.batch_size, training=True):
+                    batch_loss, _, tb_op = session.run([self.loss,
+                                                        self.train_step,
+                                                        self.tensorboard_op],
+                                                       feed_dict={self.x: batch_x,
+                                                                  self.y: batch_y,
+                                                                  self.is_training: True})
+                    avg_loss.append(batch_loss)
+                print("Average Loss for epoch {} = {}.".format(e + 1, sum(avg_loss) / len(avg_loss)))
+
+                if (e + 1) % log_step == 0:
+                    avg_loss = []
+                    avg_accuracy = []
+                    for batch_x, batch_y in data.dataset.next_batch(self.batch_size, validate=True):
+                        batch_loss, batch_acc = session.run([self.loss,
+                                                             self.accuracy],
+                                                            feed_dict={self.x: batch_x,
+                                                                       self.y: batch_y,
+                                                                       self.is_training: False})
+                        avg_loss.append(batch_loss)
+                        avg_accuracy.append(batch_acc)
+
+                    try:
+                        avg_loss = sum(avg_loss) / len(avg_loss)
+                        avg_accuracy = sum(avg_accuracy) / len(avg_accuracy)
+                        print("Validation Loss: {}. Validation Accuracy: {}".format(avg_loss, avg_accuracy))
+                    except ZeroDivisionError:
+                        avg_loss = 1000000.0
+                        avg_accuracy = 0
+                        print("Error! The accuracy of the model on the validation set is 0.")
+
+                    if avg_loss < min_valid_loss:
+                        min_valid_loss = avg_loss
+                        with open(valid_file, 'w') as f:
+                            f.write(str(avg_loss))
+                        fname = self.saver.save(session, self.model_file)
+                        print("Session saved in {}".format(fname))
+
+                writer.add_summary(tb_op, e)
+
+            writer.close()
+
+        print("Training complete!")
+
+        # self.evaluate(data)
+
+    # def evaluate(self, data):   # evaluate方法弃用，改用test方法
+    #     with tf.Session() as session:
+    #         self.saver.restore(session, self.model_file)
+    #
+    #         avg_loss = []
+    #         avg_accuracy = []
+    #         for batch_x, batch_y in data.next_batch(batch_size=16, evaluate=True):
+    #             pred, loss_, acc_ = session.run([self.prediction,
+    #                                              self.loss,
+    #                                              self.accuracy],
+    #                                             feed_dict={self.x: batch_x,
+    #                                                        self.y: batch_y,
+    #                                                        self.is_training: False})
+    #             avg_loss.append(loss_)
+    #             avg_accuracy.append(acc_)
+    #
+    #         avg_loss = sum(avg_loss) / len(avg_accuracy)
+    #         print("Average loss on evaluation set: {}".format(avg_loss))
+    #         print("Average accuracy of evaluate set: {}".format(avg_accuracy))
+    #
+    #     return avg_loss, avg_accuracy
+
+    def test(self):
+        print("Beginning the testing process...")
+
+        data = pickle.load(open("test_dataset.pkl", 'rb'))
+
+        with tf.Session() as session:
+            self.saver.restore(session, self.model_file)
+
+            avg_loss = []
+            avg_accuracy = []
+            for batch_x, batch_y in data.dataset.next_batch(batch_size=16, test=True):
+                pred, loss_, acc_ = session.run([self.prediction,
+                                                 self.loss,
+                                                 self.accuracy],
+                                                feed_dict={
+                                                    self.x: batch_x,
+                                                    self.y: batch_y,
+                                                    self.is_training: False
+                                                })
+                avg_loss.append(loss_)
+                avg_accuracy.append(acc_)
+            avg_loss = sum(avg_loss) / len(avg_loss)
+            avg_accuracy = sum(avg_accuracy) / len(avg_accuracy)
+            print("Test complete!")
+            print("Average loss on test set: {}".format(avg_loss))
+            print("Average accuracy of test dataset: {}".format(avg_accuracy))
 
 
+if __name__ == "__main__":
+    fallDetector = FallDetector(16, 3, 180, 2)
 
+    # # 如果不需要重新加载训练数据，注释掉下面两行
+    # train_path = r"D:\OpenSim\Datasetall\Video_simu_dataset"
+    # fallDetector.load_dataset(train_path,dataloader="simu",use_type="train")
+    #
+    # # 如果不需要重新训练，注释掉下面一行
+    # fallDetector.train(epochs=50, resume=False)
 
+    # 如果不需要重新加载测试数据，注释掉下面两行
+    # test_path = r"D:\OpenSim\Datasetall\Thesis_Fall_Dataset"
+    # fallDetector.load_dataset(test_path,dataloader="real",use_type="test")
 
+    test_path = "D:/OpenSim/Datasetall/MobiFall_Dataset_v2.0"
+    fallDetector.load_dataset(test_path, dataloader="mobifall", use_type="test")
 
+    fallDetector.test()
 
-
-
-
-
-
-
-
-
-
-
+# # 真实数据与仿真数据混合，训练模型
+# # 另一种方法是先用仿真数据集训练模型，再用真实数据集训练模型（resume=True）
+# if __name__=="__main__":
+#
+#     fallDetector = FallDetector(16, 12, 180, 2)
+#
+#     simu_path=r"D:\OpenSim\Datasetall\Video_simu_dataset"
+#     xsen_path=r"D:\OpenSim\Datasetall\OUR_Fall_Dataset\IMUData\RFD_0"
+#
+#     simu_data=SimuDataLoader(simu_path)
+#     xsen_data=XsenDataLoader(xsen_path)
+#
+#     simu_feature=simu_data.dataset.features
+#     simu_label=simu_data.dataset.labels
+#     xsen_feature=xsen_data.dataset.features
+#     xsen_label=xsen_data.dataset.labels
+#
+#     features=np.concatenate((simu_feature,xsen_feature),axis=0)
+#     labels=np.concatenate((simu_label,simu_label),axis=0)
+#
+#     mix_data=MixData(features,labels)
+#
+#     pickle.dump(mix_data,open("train_dataset.pkl",'wb'))
+#
+#     fallDetector.train(epochs=50)
+#
+#     test_path = r"D:\OpenSim\Datasetall\Thesis_Fall_Dataset"
+#     fallDetector.load_dataset(test_path,dataloader="real",use_type="test")
+#     fallDetector.test()
--- utils.py
+++ utils.py
@@ -34,35 +34,40 @@ class Data:
         self.features = self.features[indices]
         self.labels = self.labels[indices]
 
-    def next_batch(self, batch_size=16, training=False, validation=False, evaluate=False, test=False):
+    def next_batch(self, batch_size=16, training=False, validate=False, evaluate=False, test=False):
         features = self.features
         labels = self.labels
 
         train_size = int(features.shape[0] * 0.8)
-        valid_size = int(features.shape[0] * 0.95)
+        # valid_size = int(features.shape[0] * 0.9)
 
         if test:
             pass
         elif training:
             features = features[:train_size]
             labels = labels[:train_size]
-        elif validation:
-            features = features[train_size:valid_size]
-            labels = labels[train_size:valid_size]
-        elif evaluate:
-            features = features[valid_size:]
-            labels = labels[valid_size:]
+
+        # evaluate是从训练集中剔出来的测试集，但是为了彻底分离训练集与测试集，弃用evaluate
+        # elif validate:
+        #     features = features[train_size:valid_size]
+        #     labels = labels[train_size:valid_size]
+        # elif evaluate:
+        #     features = features[valid_size:]
+        #     labels = labels[valid_size:]
+
+        elif validate:
+            features = features[train_size:]
+            labels = labels[train_size:]
+
         else:
             print("Error! Specify one mode!")
 
         num_samples = features.shape[0]
-        n_batches = int(np.ceil(num_samples / batch_size))
+        n_batches = int(np.floor(num_samples / batch_size))
 
         for b in tqdm(range(n_batches), file=sys.stdout):
             start = b * batch_size
             end = start + batch_size
-            if end >= num_samples:
-                continue
             feature_batch = features[start:end]
             label_batch = labels[start:end]
 
@@ -107,7 +112,7 @@ class SimuDataLoader:
 
                 # ARG select imus on which body segment
                 # segments= ["torso","pelvis","femur_r","tibia_r"]
-                segments = ["tibia_r"]
+                segments = ["torso"]
 
                 each_data = [[]]
                 for segment in segments:
@@ -223,7 +228,7 @@ class RealDataLoader:
         imu_files = getsub(txt_folder)
 
         # ARG 指定实验中IMU传感器的贴放部位，与txt文件一一对应
-        imu_body = {'340540.txt': 'neck',
+        imubody_map = {'340540.txt': 'neck',
                     '340539.txt': 'chest',
                     '340537.txt': 'wrist',
                     '340535.txt': 'waist',
@@ -232,13 +237,13 @@ class RealDataLoader:
         body_data = {}
         for imu_file in imu_files:
             imu_index = os.path.basename(imu_file)
-            body_data[imu_body.get(imu_index)] = self._get_imudata(imu_file)
+            body_data[imubody_map.get(imu_index)] = self._get_imudata(imu_file)
 
         pertrial_imudata = [[]]
 
         # ARG 选择哪些部位的IMu数据
-        # for segment in ["shank"]:
-        for segment in ["chest", "waist", "thigh", "shank"]:
+        for segment in ["chest"]:
+        # for segment in ["chest", "waist", "thigh", "shank"]:
             segment_data = body_data[segment]
             try:
                 pertrial_imudata = np.concatenate((pertrial_imudata, segment_data), axis=1)
@@ -294,9 +299,9 @@ class RealDataLoader:
 
 
 class XsenDataLoader:
-    def __init__(self, path='', sensor_num=7):
+    def __init__(self, path=''):
         self.path = path
-        self.sensor_num = sensor_num
+        self.sensor_num = 4
         self.dataset = {}
         self._load_data()
         self._format_dataset()
@@ -341,7 +346,7 @@ class XsenDataLoader:
 
     def _prepare_dataset_from_dir(self, each_test):
         # ARG: 佩戴Xsens传感器采集的imu数据
-        imu_body = {'175': 'chest',
+        imubody_map = {'175': 'chest',
                     '1D1': 'waist',
                     '203': 'thigh',
                     '205': 'shank',
@@ -351,7 +356,7 @@ class XsenDataLoader:
         body_data = {}
         for imu_file in each_test:
             imu_index = (os.path.basename(imu_file))[-7:-4]
-            body_data[imu_body.get(imu_index)] = self._get_imudata(imu_file)
+            body_data[imubody_map.get(imu_index)] = self._get_imudata(imu_file)
 
         pertrial_imudata = [[]]
 
@@ -393,6 +398,185 @@ class XsenDataLoader:
             features = self.dataset[key]
             num_samples = features.shape[0]
             labels.extend(num_samples * [mot_type_num])
+            try:
+                readings = np.concatenate((readings, features), axis=0)
+            except ValueError:
+                readings = features
+        labels = np.array(labels)
+        self.dataset = Data(readings, labels)
+
+
+class MobiFallDataLoader:
+    """
+    Loads data from MobiFall_Dataset_v2.0/ directory.
+    The result is stored in the dictionary.
+    """
+
+    def __init__(self, path=''):
+        self.path = path
+        self.sensor_num=3
+        self.ADL_TYPES = ["CSI", "CSO", "JOG", "JUM", "SCH", "STD", "STN", "STU", "WAL"]
+        self.FALL_TYPES = ["BSC", "FKL", "FOL", "SDL"]
+        self.classes = ["CSI", "CSO", "JOG", "JUM", "SCH", "STD", "STN", "STU", "WAL", "BSC", "FKL", "FOL", "SDL"]
+        self.dataset = {}
+        self._load_data()
+        self._format_dataset()
+
+    def _load_data(self):
+        '''
+        Load the dataset, starting from root directory `self.path`.
+        '''
+        print("Loading dataset...")
+        subdirs = self._get_subdirs()
+
+        ADLs = []
+        FALLs = []
+        for i in range(len(subdirs)):
+            subdir = subdirs[i]
+            class_dirs = self._get_subdirs(subdir)
+            for class_dir in class_dirs:
+                if class_dir.endswith('ADL'):
+                    ADLs.append(class_dir)
+                elif class_dir.endswith('FALLS'):
+                    FALLs.append(class_dir)
+                else:
+                    print("Found unusual directory:", class_dir)
+        self._process_category(ADLs, self.ADL_TYPES)
+        self._process_category(FALLs, self.FALL_TYPES)
+
+    def _process_category(self, dirs=[], category_types=[]):
+        '''
+        Process ADL/FALL directories to load data.
+        '''
+        for i in tqdm(range(len(dirs))):
+            dir_ = dirs[i]
+            subdirs = self._get_subdirs(dir_)
+            assert len(subdirs) == len(category_types), "Found an unusual class in " + dir_
+
+            for subdir in subdirs:
+                class_ = self._get_class(subdir)
+                data = self._prepare_dataset_from_dir(subdir)
+
+                if data.shape[0] == 0:
+                    # MobiFall_Dataset_v2.0/sub21/FALLS/SDL is empty; deal with this here.
+                    continue
+
+                try:
+                    self.dataset[class_] = np.concatenate((self.dataset[class_], data), axis=0)
+                except KeyError:
+                    self.dataset[class_] = data
+
+    def _prepare_dataset_from_dir(self, folder=''):
+        '''
+        Prepares dataset from .txt files within `folder`.
+        '''
+        files = self._get_trails(folder)
+        files = [file for file in files if "acc" in file]
+        print(files)
+        num_trails = len(files)
+        num_trails_per_sensor = num_trails // self.sensor_num
+
+        data = []
+        for i in range(num_trails_per_sensor):
+            data_files = []
+            for s in range(self.sensor_num):
+                data_files.append(files[i + s * num_trails_per_sensor])
+            data.append(self._accumulate(data_files))
+
+        return np.array(data)
+
+    def _accumulate(self, files=[]):
+        '''
+        Args:
+            files = A list of files containing individual sensor readings
+        Return:
+            Accumulated results as a numpy array
+        '''
+        assert len(files) >= 1
+        features = self._read_file(files[0], interpolate=True)
+        for i in range(1, len(files)):
+            f = self._read_file(files[1], interpolate=True)
+            features = np.concatenate((features, f), axis=1)
+        return features
+
+    def _read_file(self, fname='', interpolate=False):
+        assert fname != ''
+        data = ''
+        with open(fname) as file:
+            data = file.read().split('\n')
+        data = data[17:-1]  # skip header lines and ending '' line
+        feature = []
+        for line in data:
+            info = line.split(',')
+            info = list(map(float, info))
+            feature.append(info[1:])
+        feature = np.array(feature)
+        if interpolate:
+            return self._interpolate_sensor_data(feature)
+        return feature
+
+    def _interpolate_sensor_data(self, features):
+        '''
+        Change frequency of sensor data `features` to `samples` number of samples.
+        Args:
+            features = A 2-D Numpy array containing sensor values for several time-steps.
+            samples  = Number of samples to choose.
+        Return:
+            Feature values interpolated to `samples` number of samples.
+        '''
+
+        # resample a 1d col to `samples` number of samples.
+        interpolate_1d = lambda col: signal.resample(col, TIME_STEPS)
+
+        # Apply interpolation to each column in `features` argument.
+        return np.apply_along_axis(interpolate_1d, 0, features)
+
+    def _get_class(self, subdir=''):
+        '''
+        Returns parent directory name, representing the class.
+        '''
+        return os.path.basename(os.path.normpath(subdir))
+
+    def _get_files(self, dir_name='', sort=False):
+        files = [os.path.join(dir_name, file) for file in os.listdir(dir_name)]
+        if sort:
+            assert len(files) // self.sensor_num < 10, "Add functionality for >=10 trails"
+            return sorted(files)
+        return files
+
+    def _get_trails(self, folder=''):
+        files = self._get_files(folder, sort=True)
+        assert len(files) % self.sensor_num == 0, "Inconsistent number of sensors in " + folder
+
+        return files
+
+    def _get_subdirs(self, dir_name=''):
+        """
+        Args:
+            dir_name = Name of the directory
+                       If dir_name == '', the code uses `self.path` as dir_name
+        Return:
+            Paths to sub directories within `dir_name`.
+        """
+        if dir_name == '':
+            dir_name = self.path
+        subdirs = os.listdir(dir_name)
+        subdirs = [os.path.join(dir_name, sd) for sd in subdirs]
+        return list(filter(os.path.isdir, subdirs))
+
+    def _format_dataset(self):
+        print("Formatting dataset...")
+        readings = None
+        labels = []
+        for key in self.dataset:
+            class_no = self.classes.index(key)
+            if class_no >= 9:
+                class_no = 1
+            else:
+                class_no = 0
+            features = self.dataset[key]
+            num_samples = features.shape[0]
+            labels.extend(num_samples * [class_no])
             try:
                 readings = np.concatenate((readings, features), axis=0)
             except ValueError:
