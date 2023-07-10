4876ce6ea9a4a560b4397159709f8edb4b3884c4
tangjie <tangjie1953479@tongji.edu.cn>
2023-07-10 16:42:27 +0800

    20230708: v0.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # IMU_FallDetection_LSTM
 Use LSTM to detect falls, IMU data, binary classification
+
+下载后，在项目根目录下创建saved_model和for_tensorboard两个文件夹,运行main.py
--- test.py
+++ test.py
@@ -1,22 +1,5 @@
 # print("hello github")
-# import numpy as np
-#
-# each_data=[[2,3,4,5]]
-# test_array=np.array([[1,2,3,4]])
-#
-# print(test_array.shape)
-# each_data=np.concatenate((each_data,test_array),axis=1)
-# print(each_data)
-# each_data = np.expand_dims(each_data, axis=2)
-# print(each_data)
 
-import pandas as pd
-import numpy as np
-from numpy import nan as NaN
-df=pd.DataFrame([[1,2,3],[NaN,NaN,2],[NaN,NaN,NaN],[8,8,NaN]])
-print(df)
-df1=df.fillna(method="pad")
-print(df1)
 
 
 
